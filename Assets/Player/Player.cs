0514698b80786c158443832573743b2b76a3505f
ty cobb <bin@tycobb.club>
2024-07-03 16:49:13 -0400

    fix bouncing rotation

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -12,8 +12,8 @@ sealed class Player: MonoBehaviour {
 
     // -- refs --
     [Header("refs")]
-    [Tooltip("the character controller")]
-    [SerializeField] CharacterController m_Controller;
+    [Tooltip("the physics body")]
+    [SerializeField] Rigidbody m_Body;
 
     // -- props --
     /// the current timer for the move action
@@ -28,30 +28,40 @@ sealed class Player: MonoBehaviour {
     /// the initial rotation when the move began
     Quaternion m_Move_Rotation_From;
 
-    /// the initial rotation when the move began
+    /// the final rotation of the move
     Quaternion m_Move_Rotation_To;
+
+    /// the current rotation of the move
+    Quaternion m_Move_Rotation_Curr;
+
+    /// the rotation reflection from hitting a wall
+    Quaternion m_Move_Rotation_Reflect;
+
+    /// the current timer for the move rotation
+    Vector3 m_Move_Rotation_InitialForward;
 
     // -- lifecycle --
     void Start() {
         m_Move = new EaseTimer();
         m_Move_Rotation = new EaseTimer(new(0f, m_Tuning.Move_Rotation_Curve));
 
-        // apply the controller's position offset
-        transform.position += m_Controller.skinWidth * Vector3.up;
+        m_Move_Rotation_Curr = Quaternion.identity;
+        m_Move_Rotation_Reflect = Quaternion.identity;
+        m_Move_Rotation_InitialForward = transform.forward;
     }
 
     void Update() {
         var isMoveJustPressed = Keyboard.current.spaceKey.wasPressedThisFrame;
         if (m_Move.IsInactive && isMoveJustPressed) {
-            var nextForward = Random.insideUnitCircle.normalized.XZ();
-
             // start move
             m_Move.Duration = m_Tuning.Move_Duration.Sample();
             m_Move.Start();
 
             // start move rotation
-            m_Move_Rotation_From = transform.rotation;
-            m_Move_Rotation_To = Quaternion.LookRotation(nextForward, Vector3.up);
+            var nextForward = Random.insideUnitCircle.normalized.XZ();
+            m_Move_Rotation_From = m_Move_Rotation_Curr;
+            m_Move_Rotation_To = m_Move_Rotation_From * Quaternion.LookRotation(nextForward, Vector3.up);
+
             m_Move_Rotation.Duration = m_Tuning.Move_Rotation_Duration.Sample();
             m_Move_Rotation.Start();
         }
@@ -59,19 +69,54 @@ sealed class Player: MonoBehaviour {
 
     void FixedUpdate() {
         var delta = Time.deltaTime;
-        var trs = transform;
 
+        // while active, move pos
         if (m_Move.TryTick()) {
-            var moveSpeed = m_Tuning.Move_Speed.Evaluate(m_Move.Pct);
-            m_Controller.Move(moveSpeed * delta * trs.forward);
+            var currPos = m_Body.position;
+            var moveLen = m_Tuning.Move_Speed.Evaluate(m_Move.Pct) * delta;
+            var nextPos = currPos + moveLen * transform.forward;
+
+            m_Body.MovePosition(nextPos);
         }
 
+        // while active, rotate along move curve
         if (m_Move_Rotation.TryTick()) {
-            trs.rotation = Quaternion.Slerp(
+            var nextMoveRot = Quaternion.Slerp(
                 m_Move_Rotation_From,
                 m_Move_Rotation_To,
                 m_Move_Rotation.Pct
             );
+
+            m_Move_Rotation_Curr = nextMoveRot;
         }
+
+        // always set current rotation
+        var nextRot = (
+            m_Move_Rotation_Curr *
+            m_Move_Rotation_Reflect
+        );
+
+        m_Body.MoveRotation(nextRot);
+    }
+
+    // -- events --
+    void OnCollisionEnter(Collision other) {
+        // ignore everything besides first contact (bad idea?
+        var contact = other.GetContact(0);
+
+        // get current forward based on accumulated rotation (relevant if multiple
+        // collisions in a single frame?)
+        var currRotation = m_Move_Rotation_Reflect * m_Move_Rotation_Curr;
+        var currFwd = currRotation * m_Move_Rotation_InitialForward;
+
+        // reflect direction over the normal (bounce)
+        var dirNormal = contact.normal;
+        var dirCross = Vector3.ProjectOnPlane(currFwd, dirNormal);
+        var dirInline = currFwd - dirCross;
+        var dirReflect = dirCross - dirInline;
+
+        // accumulate the reflection
+        var rotation = Quaternion.FromToRotation(currFwd, dirReflect);
+        m_Move_Rotation_Reflect *= rotation;
     }
 }
--- Assets/Plugins/Soil/Runtime/Logging/Log.cs
+++ Assets/Plugins/Soil/Runtime/Logging/Log.cs
@@ -3,6 +3,7 @@ namespace Soil {
 /// a set of log tags
 public static class Log {
     public static readonly Tag System = new("system", "#535535");
+    public static readonly Tag Temp   = new("temppp", "#7e3444");
 }
 
 }
