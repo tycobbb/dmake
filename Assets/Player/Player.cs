10edee32a320701cb272f0ca232f29c40ad2b7e1
ty cobb <bin@tycobb.club>
2024-07-05 12:03:56 -0400

    fix player facing

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -49,8 +49,12 @@ sealed class Player: MonoBehaviour {
     /// the initial forward direction
     Vector3 m_InitialForward;
 
+    /// the initial rotation
+    Quaternion m_InitialRotation;
+
     // -- lifecycle --
     void Start() {
+
         m_Move = new EaseTimer();
 
         m_Move_Rotation = new EaseTimer(new(0f, m_Tuning.Move_Rotation_Curve));
@@ -59,7 +63,9 @@ sealed class Player: MonoBehaviour {
         m_Move_Reflect = new EaseTimer(m_Tuning.Move_Reflect);
         m_Move_Reflect_Curr = Quaternion.identity;
 
-        m_InitialForward = transform.forward;
+        var trs = transform;
+        m_InitialForward = trs.forward;
+        m_InitialRotation = trs.rotation;
     }
 
     void Update() {
@@ -70,13 +76,13 @@ sealed class Player: MonoBehaviour {
             m_Move.Start();
 
             // accumulate the move rotation
-            var currRotation = m_Move_Rotation_Curr;
+            var currMoveRot = m_Move_Rotation_Curr;
             var nextForward = Random.insideUnitCircle.normalized.XZ();
-            var nextRotation = currRotation * Quaternion.LookRotation(nextForward, Vector3.up);
+            var nextMoveRot = currMoveRot * Quaternion.LookRotation(nextForward, Vector3.up);
 
             // start the move rotation
-            m_Move_Rotation_Src = currRotation;
-            m_Move_Rotation_Dst = nextRotation;
+            m_Move_Rotation_Src = currMoveRot;
+            m_Move_Rotation_Dst = nextMoveRot;
             m_Move_Rotation.Duration = m_Tuning.Move_Rotation_Duration.Sample();
             m_Move_Rotation.Start();
         }
@@ -85,10 +91,15 @@ sealed class Player: MonoBehaviour {
     void FixedUpdate() {
         var delta = Time.deltaTime;
 
+        var currMoveRot = m_Move_Rotation_Curr;
+        var currReflectRot = m_Move_Reflect_Curr;
+        var nextMoveRot = currMoveRot;
+        var nextReflectRot = currReflectRot;
+
         // while active, move pos (towards reflection destination)
         if (m_Move.TryTick()) {
             var currPos = m_Body.position;
-            var currFwd = m_Move_Rotation_Curr * m_Move_Reflect_Dst * m_InitialForward;
+            var currFwd = currMoveRot * currReflectRot * m_InitialForward;
             var moveLen = m_Tuning.Move_Speed.Evaluate(m_Move.Pct) * delta;
             var nextPos = currPos + moveLen * currFwd;
 
@@ -97,32 +108,31 @@ sealed class Player: MonoBehaviour {
 
         // while active, rotate along move curve
         if (m_Move_Rotation.TryTick()) {
-            var nextMoveRot = Quaternion.Slerp(
+            nextMoveRot = Quaternion.Slerp(
                 m_Move_Rotation_Src,
                 m_Move_Rotation_Dst,
                 m_Move_Rotation.Pct
             );
-
-            m_Move_Rotation_Curr = nextMoveRot;
         }
 
         // while active, reflect off of collision
         if (m_Move_Reflect.TryTick()) {
-            var nextReflectRot = Quaternion.Slerp(
+            nextReflectRot = Quaternion.Slerp(
                 m_Move_Reflect_Src,
                 m_Move_Reflect_Dst,
                 m_Move_Reflect.Pct
             );
-
-            m_Move_Reflect_Curr = nextReflectRot;
         }
 
         // rotate to interpolated rotation
         var nextRot = (
-            m_Move_Rotation_Curr *
-            m_Move_Reflect_Curr
+            nextMoveRot *
+            nextReflectRot *
+            m_InitialRotation
         );
 
+        m_Move_Rotation_Curr = nextMoveRot;
+        m_Move_Reflect_Curr = nextReflectRot;
         m_Body.MoveRotation(nextRot);
     }
 
